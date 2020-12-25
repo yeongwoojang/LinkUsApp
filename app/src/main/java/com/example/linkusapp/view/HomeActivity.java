3d1610e20a8651891512932eb4793a41a3a0f9f0
jgh6272 <jgh6272@naver.com>
2020-12-26 03:38:24 +0900

    12/26 Login Activity UI(완료), Kakao Login (완료),facebook (진행중) (zibro)

--- app/src/main/java/com/example/linkusapp/view/HomeActivity.java
+++ app/src/main/java/com/example/linkusapp/view/HomeActivity.java
@@ -4,8 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -22,6 +27,8 @@ import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.response.MeV2Response;
 import com.kakao.util.exception.KakaoException;
+
+import java.security.MessageDigest;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -103,6 +110,9 @@ public class HomeActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(MeV2Response result) {
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Log.d("asdasd", "onSuccess: "+result.getNickname());
+                    Log.d("asdasd", "onSuccess: "+result.getId());
+
                     intent.putExtra("name", result.getNickname());
                     intent.putExtra("profile", result.getProfileImagePath());
                     startActivity(intent);
@@ -116,4 +126,20 @@ public class HomeActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "로그인 도중 오류가 발생했습니다. 인터넷 연결을 확인해주세요: "+e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
+
+  /*  private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }*/
 }
