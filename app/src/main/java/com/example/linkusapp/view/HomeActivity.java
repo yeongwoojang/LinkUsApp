5ff4b940b21855dae334895b0f081c0716e2e8fd
jgh6272 <jgh6272@naver.com>
2020-12-25 16:13:51 +0900

    12/25 HomeActivity UI

--- app/src/main/java/com/example/linkusapp/view/HomeActivity.java
+++ app/src/main/java/com/example/linkusapp/view/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.linkusapp;
+package com.example.linkusapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.linkusapp.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
