952ec3283afdd8c25e4ceb3f14e5223fb5e25b84
yeongwoo <sbtmxhs@gmail.com>
2020-12-30 22:46:11 +0900

    12/30(ZiBro브랜치랑 병합)

--- app/src/main/java/com/example/linkusapp/view/AddUserInfoActivity.java
+++ app/src/main/java/com/example/linkusapp/view/AddUserInfoActivity.java
@@ -12,5 +12,6 @@ public class AddUserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_info);
+
     }
 }
