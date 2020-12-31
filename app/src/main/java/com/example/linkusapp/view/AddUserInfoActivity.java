ef4925facfa13b8b84fea55252ce042c42ede683
jgh6272 <jgh6272@naver.com>
2020-12-31 22:47:57 +0900

    12/31 2차 회원가입(성별,닉네임,주소) -ing

--- app/src/main/java/com/example/linkusapp/view/AddUserInfoActivity.java
+++ app/src/main/java/com/example/linkusapp/view/AddUserInfoActivity.java
@@ -2,16 +2,26 @@ package com.example.linkusapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.linkusapp.R;
 
 public class AddUserInfoActivity extends AppCompatActivity {
 
+    private Button nextBtn,preBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_info);
+
+        nextBtn = (Button)findViewById(R.id.next_btn);
+        preBtn = (Button)findViewById(R.id.previous_btn);
+
+        nextBtn.setPaintFlags(nextBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        preBtn.setPaintFlags(preBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
 
     }
 }
