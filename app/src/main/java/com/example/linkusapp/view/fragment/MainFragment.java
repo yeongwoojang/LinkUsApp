0f3b36a443b425ced9f7be7bfdb4829176acaf7f
yeongwoojang <sbtmxhs@gmail.com>
2021-02-20 03:57:08 +0900

    2/20 채팅 기능 시작

--- app/src/main/java/com/example/linkusapp/view/fragment/MainFragment.java
+++ app/src/main/java/com/example/linkusapp/view/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.linkusapp.view.fragment;
 
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
@@ -13,25 +14,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.linkusapp.R;
+import com.example.linkusapp.view.activity.ChatActivity;
 import com.example.linkusapp.view.custom.RoundImageView;
 
 public class MainFragment extends Fragment {
 
     private RoundImageView logoImage;
+    TextView testChat;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        logoImage = view.findViewById(R.id.logo_image);
+        testChat = view.findViewById(R.id.test_chat);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        logoImage.setRectRadius(500f);
+        testChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ChatActivity.class));
+            }
+        });
 //        GradientDrawable drawable = (GradientDrawable)getActivity().getDrawable(R.drawable.form_round_img);
 //        logoImage.setBackground(new ShapeDrawable(new OvalShape()));
 //        logoImage.setClipToOutline(true);
