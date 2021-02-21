a122e61cf51a65f842f2af4b092e82980f1b13fe
jgh6272 <jgh6272@naver.com>
2021-02-22 01:39:44 +0900

    2/22 게시판 댓글 보내기 완료. 게시판 댓글현황 띄우기 진행중

--- app/src/main/java/com/example/linkusapp/view/adapter/CommentAdapter.java
+++ app/src/main/java/com/example/linkusapp/view/adapter/CommentAdapter.java
@@ -1,33 +1,60 @@
 package com.example.linkusapp.view.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.linkusapp.R;
+import com.example.linkusapp.model.vo.Comment;
+import com.example.linkusapp.viewModel.CommentViewModel;
+
+import java.util.List;
+
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
 
+    private List<Comment> mDataset;
+    private CommentViewModel viewModel;
     public class CommentViewHolder extends RecyclerView.ViewHolder{
+        private TextView writerTv,writingTimeTv,commentTv;
+        private Button recommentBtn;
         public CommentViewHolder(@NonNull View itemView) {
             super(itemView);
+            writerTv= itemView.findViewById(R.id.item_id_tv);
+            writingTimeTv= itemView.findViewById(R.id.item_time_tv);
+            commentTv = itemView.findViewById(R.id.item_comment_tv);
+            recommentBtn = itemView.findViewById(R.id.item_reply_btn);
         }
     }
-
+    public void updateItem(List<Comment> items){
+        mDataset = items;
+        notifyDataSetChanged();
+    }
+    public CommentAdapter(List<Comment> commentList, CommentViewModel viewModel){
+        mDataset = commentList;
+        this.viewModel = viewModel;
+    }
     @NonNull
     @Override
-    public CommentAdapter.CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_comment,parent,false);
+        return new CommentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentAdapter.CommentViewHolder holder, int position) {
-
+        Comment comment = mDataset.get(position);
+        holder.writerTv.setText(comment.getbWriter());
+        holder.commentTv.setText(comment.getbComment());
+        holder.writingTimeTv.setText(comment.getbWriteTime());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mDataset.size();
     }
 }
