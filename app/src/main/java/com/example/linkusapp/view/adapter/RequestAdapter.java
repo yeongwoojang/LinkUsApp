2009b905f549e37d2ff79d3c2c5ccd530f98f7cb
packmingyun <alsrbs12304@naver.com>
2021-03-12 01:25:42 +0900

    (3/12) Data Binding

--- app/src/main/java/com/example/linkusapp/view/adapter/RequestAdapter.java
+++ app/src/main/java/com/example/linkusapp/view/adapter/RequestAdapter.java
@@ -16,6 +16,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.linkusapp.R;
+import com.example.linkusapp.databinding.ItemMemberBinding;
+import com.example.linkusapp.databinding.ItemRequestBinding;
 import com.example.linkusapp.model.vo.LeaderGroup;
 import com.example.linkusapp.model.vo.User;
 import com.example.linkusapp.viewModel.ManageJoinViewModel;
@@ -23,24 +25,27 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
-public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestViewHolder> implements View.OnClickListener{
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestViewHolder>{
+
+    private RequestAdapter thisobject = this;
 
      private List<User> items;
      private ManageJoinViewModel viewModel;
      private Context mContext;
     private Toast toast;
     private String gName ="";
-    public class RequestViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView nickNameText, ageText, genderText, addressText;
-        private ImageButton acceptBt;
-        public RequestViewHolder(@NonNull View itemView) {
-            super(itemView);
-            nickNameText = itemView.findViewById(R.id.user_nickname);
-            ageText = itemView.findViewById(R.id.user_age);
-            genderText = itemView.findViewById(R.id.user_gender);
-            addressText = itemView.findViewById(R.id.user_address);
-            acceptBt = itemView.findViewById(R.id.accept_bt);
+    public class RequestViewHolder extends RecyclerView.ViewHolder{
+        private ItemRequestBinding binding;
+
+        public RequestViewHolder(ItemRequestBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.binding.setAdapter(thisobject);
+        }
+        void bind(User user, int position){
+            binding.setUser(user);
+            binding.setPosition(position);
         }
     }
 
@@ -61,19 +66,15 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestV
     @NonNull
     @Override
     public RequestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_request,parent,false);
-        return new RequestViewHolder(view);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ItemRequestBinding binding = ItemRequestBinding.inflate(inflater, parent, false);
+        return new RequestAdapter.RequestViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RequestViewHolder holder, int position) {
         User user = items.get(position);
-        holder.nickNameText.setText(user.getUserNickname());
-        holder.ageText.setText("나이 : "+ user.getAge());
-        holder.genderText.setText("성별 : "+(user.getGender().equals("M") ? "남자" : "여자"));
-        holder.addressText.setText("거주지 : "+user.getAddress());
-        holder.acceptBt.setTag(position);
-        holder.acceptBt.setOnClickListener(this);
+        holder.bind(user,position);
     }
 
     @Override
@@ -81,34 +82,32 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestV
         return items.size();
     }
 
-    @Override
-    public void onClick(View v) {
-            new AlertDialog.Builder(mContext)
-                    .setMessage("가입 요청을 수락하시겠습니까?")
-                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //가입요청한 유저를 그룹에 추가
-                            viewModel.joinGroup(gName,items.get((int)v.getTag()).getUserId(),items.get((int)v.getTag()).getUserNickname());
-                            //DB에서 요청목록 삭제
-                            viewModel.deleteRequest(gName,items.get((int)v.getTag()).getUserNickname());
+    public void acceptBtnClickEvent(int position){
+        new AlertDialog.Builder(mContext)
+                .setMessage("가입 요청을 수락하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //가입요청한 유저를 그룹에 추가
+                        viewModel.joinGroup(gName,items.get(position).getUserId(),items.get(position).getUserNickname());
+                        //DB에서 요청목록 삭제
+                        viewModel.deleteRequest(gName,items.get(position).getUserNickname());
 
+                    }
+                })
+                .setNegativeButton("거절", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //DB에서 요청목록 삭제
+                        viewModel.deleteRequest(gName,items.get(position).getUserNickname());
+                        if(toast!=null){
+                            toast.cancel();
                         }
-                    })
-                    .setNegativeButton("거절", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //DB에서 요청목록 삭제
-                            viewModel.deleteRequest(gName,items.get((int)v.getTag()).getUserNickname());
-                            if(toast!=null){
-                                toast.cancel();
-                            }
-                            toast = Toast.makeText(mContext,"가입 요청을 거절했습니다.",Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                    }).show();
+                        toast = Toast.makeText(mContext,"가입 요청을 거절했습니다.",Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }).show();
     }
-
     public void setgName(String gName) {
         this.gName = gName;
     }
